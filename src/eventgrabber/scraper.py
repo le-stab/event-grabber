cac98ac87ae326f5a597d0e951a9f4969ef539ae
le-stab <68471427+le-stab@users.noreply.github.com>
2021-02-07 15:42:28 -0300

    scraper get name, and new requer

---
+++ src/eventgrabber/scraper.py
@@ -0,0 +1,27 @@
+# https://mentalwellness.byhealthmeans.com/encore-weekend/?utm_source=ActiveCampaign&utm_medium=email&utm_content=Last+day+for+Encore+Weekend&utm_campaign=MNWL20
+
+from bs4 import BeautifulSoup
+import requests
+
+insite = input('\n\nsite: ')
+class Site:
+    def __init__(self, site):
+        self.site = site
+
+    def ini(self):
+        startpage = requests.get(self.site)
+        bsoup = BeautifulSoup(startpage.text, "html.parser")
+        buttons = bsoup.find_all('a', class_='button')
+
+        for button in buttons:
+            name = requests.get(button['href'])
+            nsoup = BeautifulSoup(name.text, "html.parser")
+
+            names = nsoup.find_all('h1', class_='presenter-title')
+
+            for speaker in names:
+                print(speaker.getText())
+
+site = Site(insite)
+
+site.ini()
